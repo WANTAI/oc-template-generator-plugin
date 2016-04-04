2aefe9aa8cad5dfe017546d445a2f96e91ec25ff
Andrey Kharanenka <a.khoronenko@lovata.com>
2016-04-04 12:58:01 +0300

    Init Plugin

--- Plugin.php
+++ Plugin.php
@@ -4,6 +4,22 @@ use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'lovata.templategenerator::lang.plugin.name',
+            'description' => 'lovata.templategenerator::lang.plugin.description',
+            'author'      => 'LOVATA',
+            'icon'        => 'oc-icon-skyatlas'
+        ];
+    }
+
     public function registerComponents()
     {
     }
